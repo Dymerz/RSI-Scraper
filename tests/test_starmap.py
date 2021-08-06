ecff5027fc5aaeec7bde219ff6c2633169a7b4c4
Urbain Corentin <corentin.urbain96@gmail.com>
2021-08-07 00:27:44 +0200

    Add unittest to StarmapRouteSearch

--- tests/test_starmap.py
+++ tests/test_starmap.py
@@ -66,3 +66,22 @@ def test_request_species():
     assert data is not None
 
     assert_valid_schema(data, 'starmap_species.json')
+
+
+@pytest.mark.starmap
+@pytest.mark.parametrize('_from, to, ship_size', [
+    ('STANTON.LZS.LORVILLE', 'STANTON.LZS.AREA18', "M"),
+    ('STANTON.LZS.LORVILLE', 'STANTON.LZS.AREA18', "L"),
+    ('STANTON.LZS.LORVILLE', 'STANTON.LZS.AREA18', "S"),
+    ('SOL.PLANETS.EARTH', 'PYRO.PLANETS.PYROI', 'L'),
+    ('ORION.PLANET.ORIONIIIARMITAGE', 'AYRKA.PLANET.AYRKAII', 'S'),
+])
+def test_request_route_search(_from, to, ship_size):
+    from rsi_scraper import StarmapRouteSearch
+
+    object = StarmapRouteSearch(_from, to, ship_size)
+    data = object.execute()
+
+    assert data is not None
+
+    assert_valid_schema(data, 'starmap_route_search.json')
